4442aada4dbf8ad1ecb3b966e6217cbaa9ac52cb
munganyendesandrine <mungasandwingsd@gmail.com>
2019-01-20 13:09:01 -0800

    bank account

---
+++ bank1.js
@@ -0,0 +1,38 @@
+function Account(time,name,deposit, withdraw, available) {
+    this.time = time;
+    this.name = name;
+    this.deposit = 0;
+    this.withdraw = 0;
+    this.available = 0;
+}
+//Deposit operation
+Account.prototype.deposit= function (deposit) {
+    var number1=document.getElementById("deposit").value;
+}
+Account.prototype.available=function(amountAvailable){
+    var number2=document.getElementById("amountAvailable2").value;
+}
+function CheckDeposit(){
+    var number1=parseInt(document.getElementById("deposit").value);
+    var number2=parseInt(document.getElementById("amountAvailable2").value);
+    var number3=number2+number1;
+    document.getElementById("new1").value=number3;
+    document.getElementById("time").value=Date();
+};
+//Withdraw operation
+Account.prototype.withdraw=function(withdraw){
+    var number5=parseInt(document.getElementById("new1").value);
+    var number4=parseInt(document.getElementById("withdraw").value);
+    
+
+function CheckWithdraw(){
+    if(number5>=number4){
+        var number6=number5-number4;
+        document.getElementById("new2").value=number6;
+        document.getElementById("time2").value=Date();
+    }
+    else{
+        alert("Sorry,You have insufficient money for withdrawing!");
+    }  
+}
+};
